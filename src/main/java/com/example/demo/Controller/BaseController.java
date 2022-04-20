8bd326e590f89293c36a17f173335ce5099726e4
RioRichard <thanh.huuphungak47@gmail.com>
2022-04-20 16:42:14 +0700

    Test a bit

---
+++ src/main/java/com/example/demo/Controller/BaseController.java
@@ -0,0 +1,8 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BaseController {
+
+}
---
+++ src/main/java/com/example/demo/Model/Product.java
@@ -0,0 +1,90 @@
+package com.example.demo.Model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Product {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    @Column(name="idproduct")
+    int idProduct;
+    @Column(name="idcategory")
+    int idCategory;
+
+    String name;
+
+    int price;
+
+    int stock;
+    @Column(name="imageurl")
+    String imageUrl;
+    @Column(name="isdelete")
+    boolean isDelete;
+    String description;
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(boolean delete) {
+        isDelete = delete;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
