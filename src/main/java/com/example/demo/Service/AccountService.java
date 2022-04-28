588fcff3e3f51a95de09bbdf0498e061a1b54cf1
RioRichard <thanh.huuphungak47@gmail.com>
2022-04-28 16:50:45 +0700

    Nothing change

--- src/main/java/com/example/demo/Service/AccountService.java
+++ src/main/java/com/example/demo/Service/AccountService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AccountService extends BaseService{
     public void AddAccount(Account model){
+        
         Accounts.saveAndFlush(model);
         
     }
